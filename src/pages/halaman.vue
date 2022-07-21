fb2d3eb408447db5730a78d47354321869c9ac12
Khairul Auni Fikri <aunifikri@gmail.com>
2022-07-21 17:02:09 +0800

    progress

--- src/pages/halaman.vue
+++ src/pages/halaman.vue
@@ -7,7 +7,7 @@
 <template>
 	<nav class="   items-center w-2/3 text-[#005564] bg-[#f2cf77]  ">
 		<div class=" flex items-center h-full">
-			<h1 class="my-5 w-1/3 mx-5 font-bold uppercase text-xl">shave</h1>
+			<h1 class="my-5 w-1/3 mx-16 font-bold uppercase text-xl">shave</h1>
 			<div>
 				<a class="font-serif font-bold mx-5" href=""> How It Wors </a>
 			</div>
@@ -22,20 +22,33 @@
 			</div>
 		</div>
 	</nav>
-	<div class="absolute  top-[66px] h-2/3 min-h-screen w-1/3 right-0 bg-red-500 ">
-		kononkdsklgnsjlngjlndf
-	</div>
-	<div class="absolute  bottom-0 h-1/3 max-h-screen w-1/3 right-0 bg-blue-500 ">
-		kononkdsklgnsjlngjlndf
-	</div>
 	<div class="bg-[#f2cf77] w-2/3 min-h-screen ">
-		<div class="flex justify-center">
-			<h3>The ultimate app that</h3>
+		<div class="">
+			<h3
+				class="flex mx-16 leading-tight tracking-wider text-5xl w-2/3 font-medium mb-8 text-[#005564] font-serif justify-start">
+				The ultimate app that help you find
+				the righ high-quality blade for your face</h3>
+		</div>
+		<div>
+			<a class="py-4 font-semibold  mx-16 my-28 uppercase text-xl font-serif text-[#f2cf77] bg-[#005564] px-24"
+				href="">get started</a>
 		</div>
 	</div>
-	<div class="">
-		<img class="max-w-screen "
+	<div class="bg-[#d66674]  right-0">
+		<img class="min-w-screen "
 			src="https://cdn.discordapp.com/attachments/989028955585675292/999128321285697606/Vector.png" alt="">
+
+	</div>
+	<div
+		class="absolute  top-[66px] h-2/4  w-1/3 text-white  leading-tight tracking-wider font-serif text-lg right-0 bg-red-500 ">
+		<h3 class=" m-16 flex justify-center ">
+		We have high-quality goomming gift for everyone.
+		you'll find gift sets, giftcards, and more!
+		</h3>
+	</div>
+	<div class="absolute  top-[365px] h-[367px]  w-1/3 right-0 bg-blue-500 ">
+		<img class=" " src="https://cdn.discordapp.com/attachments/989028955585675292/999128321285697606/Vector.png"
+			alt="">
 
 	</div>
 	<!-- <router-link :to="{ name: 'home' }" class="
