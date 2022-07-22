6f5b5072cd025bef49d47fec065bf13d93175070
Khairul Auni Fikri <aunifikri@gmail.com>
2022-07-22 14:21:10 +0800

    progress

--- src/pages/halaman.vue
+++ src/pages/halaman.vue
@@ -27,9 +27,9 @@ import Sv9 from '@/assets/img/sv9.png';
 				3
 			</div>
 	</div> -->
-		<div class="grid grid-cols-3 grid-rows-3 min-h-screen grid-flow-col">
-			<div class="col-span-2 row-span-3 bg-[#f2cf77]">
-				<nav class="   items-center text-[#005564] bg-[#f2cf77]  ">
+		<div class="grid grid-cols-3 grid-rows-5 min-h-screen grid-flow-col">
+			<div class="col-span-2 row-span-5 bg-[#f2cf77]">
+				<nav class=" flex  items-center text-[#005564] bg-[#f2cf77]  ">
 					<div class=" font-serif font-bold flex items-center h-full">
 						<a href="" class="my-5 mr-72 mx-16 uppercase text-2xl">shave</a>
 						<div>
@@ -53,14 +53,15 @@ import Sv9 from '@/assets/img/sv9.png';
 							The ultimate app that help you find
 							the righ high-quality blade for your face</p>
 					</div>
-					<div class=" transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300 ">
+					<div class=" transition ease-in-out delay-150 w-auto hover:-translate-y-1 hover:scale-110  duration-300 ">
 						<a class="hover:bg-[#3aa7aa] py-4 font-semibold  mx-16 uppercase text-xl font-serif text-[#f2cf77] bg-[#005564] px-24"
 							href="">get started</a>
 					</div>
+					<div class="flex justify-end h-72 px32"><img :src="Sv1" width=""></div>
 				</div>
 			</div>
 			<div
-				class="row-span-1  flex justify-center leading-tight tracking-wider  font-serif text-lg text-white bg-[#d66674]">
+				class="row-span-2  flex justify-center leading-tight tracking-wider  font-serif text-xl text-white bg-[#d66674]">
 				<div class=" mx-24 ">
 					<p class=" my-8  text-center  ">
 						We have high-quality goomming gift for everyone.
@@ -72,17 +73,16 @@ import Sv9 from '@/assets/img/sv9.png';
 					</div>
 				</div>
 			</div>
-			<div class="row-span-2 grid grid-cols-3 grid-rows-3  bg-yellow-500 col-span-2">
-
-				<div class=" flex justify-center  bg-red-500 "><img :src="Sv1" width=""></div>
-				<div class=" flex justify-center  bg-blue-500 "><img :src="Sv2" width=""></div>
-				<div class=" flex justify-center  bg-green-500 "><img :src="Sv3" width=""></div>
-				<div class=" flex justify-center  bg-gray-500 "><img :src="Sv4" width=""></div>
-				<div class=" flex justify-center  bg-purple-500 "><img :src="Sv5" width=""></div>
-				<div class=" flex justify-center  bg-amber-500 "><img :src="Sv6" width=""></div>
-				<div class=" flex justify-center  bg-cyan-500 "><img :src="Sv7" width=""></div>
-				<div class=" flex justify-center  bg-blue-500 "><img :src="Sv8" width=""></div>
-				<div class=" flex justify-center  bg-sky-500 "><img :src="Sv9" width=""></div>
+			<div class="row-span-3 grid grid-cols-3 grid-rows-3  bg-yellow-500 col-span-2">
+				<div class=" flex justify-center  bg-[#ffdcec] "><img :src="Sv1" width=""></div>
+				<div class=" flex justify-center  bg-[#93dbde] "><img :src="Sv2" width=""></div>
+				<div class=" flex justify-center  bg-[#ffb0d0] "><img :src="Sv3" width=""></div>
+				<div class=" flex justify-center  bg-[#c0e6c5] "><img :src="Sv4" width=""></div>
+				<div class=" flex justify-center  bg-[#fb8b98] "><img :src="Sv5" width=""></div>
+				<div class=" flex justify-center  bg-[#c2ebed] "><img :src="Sv6" width=""></div>
+				<div class=" flex justify-center  bg-[#ffdcec] "><img :src="Sv7" width=""></div>
+				<div class=" flex justify-center  bg-[#93dbde] "><img :src="Sv8" width=""></div>
+				<div class=" flex justify-center  bg-[#fee797] "><img :src="Sv9" width=""></div>
 			</div>
 		</div>
 	</div>
